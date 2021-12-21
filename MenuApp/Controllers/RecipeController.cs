6ea1aa528e193fbf2bab8b56bdb02793cb7cb530
ViktoriaMelnyk <72021306+ViktoriaMelnyk@users.noreply.github.com>
2021-12-21 19:00:24 +0100

    DI, Passing Data to the Recipe Controller, displaying data

---
+++ MenuApp/Controllers/RecipeController.cs
@@ -0,0 +1,27 @@
+﻿using MenuApp.Data;
+using MenuApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MenuApp.Controllers
+{
+    public class RecipeController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public RecipeController(ApplicationDbContext db)
+        {
+            _db = db; 
+        }
+        public IActionResult Recipe()
+
+        {
+            IEnumerable<Recipe> objList = _db.Recipes;
+            //var randomRecipes = _db.Recipes.Take(4);
+            return View(objList);
+        }
+    }
+}
